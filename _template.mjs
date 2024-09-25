086689f585d7db45bb0dc63fc653f96a1bc9ca38
c-kick <hnldesign@hnldesign.nl>
2024-09-25 11:48:11 +0200

    Added annotations to the module template

--- _template.mjs
+++ _template.mjs
@@ -1,23 +1,42 @@
 /**
- * Module template example
+ * Module template - use this as a boilerplate for writing your own modules. Then import them into your frontend by
+ * assigning them to a node using the data-requires attribute.
+ *
+ * E.g. save it as myfirstmodule.mjs, and then add it with <div data-requires="myfirstmodule.mjs">
+ * (after following the basic usage instructions @ https://github.com/c-kick/js-modules?tab=readme-ov-file#basic-usage)
  */
 
-import {isVisible} from "./hnl.helpers.mjs";
-import eventHandler from "./hnl.eventhandler.mjs";
+/**
+ * Imports, for example purposes
+ */
+import {isVisible} from "./hnl.helpers.mjs"; //a function
+import eventHandler from "./hnl.eventhandler.mjs"; //a class
+import {hnlLogger} from "./hnl.logger.mjs"; //an object
+
+/**
+ * The name for this module, used in logging and identifying dynamically loaded modules
+ * @type {string}
+ */
+export const NAME = 'exampleModule';
 
 /**
  * init
- * Exported function that is called (if present) when the module has imported via the data-reqruires method
+ * Exported function that is called (if present) when the module has been imported via the data-requires method,
+ * as described in, and handled by, the hnl.dynamicimports module.
  * @param elements {object} Holds *all* DOM elements that had 'data-requires' specified for this module
  * 'this' will be the module object context
  */
-export const NAME = 'exampleModule';
 export function init(elements){
+  /**
+   * Do stuff here. You can safely assume the page is ready now, as the importing of dynamically loaded modules depends
+   * on reading data-attributes, which can only be safely traversed and read then the page is ready,
+   * as handled in the 'docReady' handler of the eventHandler module.
+   */
 
   /**
    example function that takes all the elements (those with 'data-requires' set for this module) and checks if they
-   visible on each scroll/resize. This one is useful for doing fancy things like fading-in/-up elements as soon as
-   they enter the user's view
+   are visible on each scroll/resize. This is useful for doing fancy things like fading-in/-up elements as soon as
+   they enter the user's view.
    @uses docShift
    @uses isVisible
    */
@@ -25,7 +44,7 @@ export function init(elements){
     elements.forEach(function(element){
       isVisible(element, function(visible) {
         if (visible) {
-          console.log(element, visible);
+          hnlLogger.log(NAME, `${element} visible? ${(visible ? 'Yes' : 'No')}`);
         }
       })
     })
